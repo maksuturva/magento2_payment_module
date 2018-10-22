4d8fcd9eab4c3003c72e9a526fb58faaea5f341e
Erik Pellikka <erik.pellikka@piimega.fi>
2018-10-22 10:15:10 +0300

    added interface preference and fixed placeshipmentafter constructor

--- app/code/Piimega/Maksuturva/Model/Observer/PlaceShipmentAfter.php
+++ app/code/Piimega/Maksuturva/Model/Observer/PlaceShipmentAfter.php
@@ -5,22 +5,6 @@ use Magento\Framework\Event\ObserverInterface;
 
 class PlaceShipmentAfter implements ObserverInterface
 {
-    protected $_maksuturvaHelper;
-    protected $_registry;
-    protected $_request;
-    protected $_maksuturvaModel;
-
-    public function __construct (
-        \Piimega\Maksuturva\Helper\Data $maksuturvaHelper,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\App\Request\Http $request,
-        \Piimega\Maksuturva\Model\PaymentAbstract $maksuturvaModel
-    ) {
-        $this->_registry = $registry;
-        $this->_request = $request;
-        $this->_maksuturvaHelper = $maksuturvaHelper;
-        $this->_maksuturvaModel = $maksuturvaModel;
-    }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
